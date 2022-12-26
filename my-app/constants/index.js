b0f29ea54674f3ca5d4976653e78cfd89426af9a
NghiaPP <phunghia91bkdn@gmail.com>
2022-12-26 13:56:08 +0700

    first commit

---
+++ my-app/constants/index.js
@@ -0,0 +1,67 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x87b6843bb10310778a57c2d47a930afdc64d5d26";
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "_maxWhitelistedAddresses",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxWhitelistedAddresses",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAddressesWhitelisted",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelistedAddresses",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
